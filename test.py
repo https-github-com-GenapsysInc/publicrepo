5e398b8b7ac317c00628121104e8edb60c406833
ravi-karan-anand <87345664+ravi-karan-anand@users.noreply.github.com>
2021-10-28 17:51:19 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+class First_Func:
+    def func(self):
+        print("This is the first function!")
+        
+def main():
+    obj=First_Func()
+    obj.func()
+        
+if __name__=='__main__':
+        main()
